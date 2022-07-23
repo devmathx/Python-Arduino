e7e41f5fc3d92a4d3a5f13c9d05ea3c63264227d
Matheus Santos <96088072+devmathx@users.noreply.github.com>
2022-07-23 15:59:24 -0300

    Criação da sketch.py
    Arquivo principal do projeto.

---
+++ sketch.py
@@ -0,0 +1,14 @@
+# Versão 0.01
+from pyfirmata import Arduino
+
+placa = Arduino("COM3")
+
+while True:
+    porta = int(input('Selecione a porta: [8,9,10] '))
+    quest = str(input('Controle o LED: '))
+    if quest == 'ligar': placa.digital[porta].write(1)
+    elif quest == 'desligar': placa.digital[porta].write(0)
+    elif quest == 'sair': break
+    else: print('Comando não reconhecido')
+        
+placa.exit()
