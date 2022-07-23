b772b141b36943a6286fbcc732eeb0790e6e04ab
Matheus Santos <96088072+devmathx@users.noreply.github.com>
2022-07-23 17:29:05 -0300

    Trabalhando no Loop
    Fazendo alterações nas saídas enquanto dentro de um Loop.

--- sketch.py
+++ sketch.py
@@ -1,14 +1,17 @@
-# Versão 0.02
+# Versão 0.029
 from pyfirmata import Arduino
 
 placa = Arduino("COM3")
 
+pin = int(input('Selecione a porta: [9,10] '))
+porta = placa.get_pin(f"d:{pin}:p")
+
 while True:
-    porta = int(input('Selecione a porta: [8,9,10] '))
-    quest = str(input('Controle o LED: '))
-    if quest == 'ligar': placa.digital[porta].write(1)
-    elif quest == 'desligar': placa.digital[porta].write(0)
-    elif quest == 'sair': break
-    else: print('Comando não reconhecido') 
+    value = str(input('Controle o LED: ')) 
+    if value == 'sair': break
+    elif value == 'pin': 
+        pin = int(input('Selecione a porta: [9,10] '))
+        porta = placa.get_pin(f"d:{pin}:p")
+    else: porta.write(float(value) / 100)
         
 placa.exit()
